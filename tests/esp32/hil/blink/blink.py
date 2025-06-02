e193dd8e33178745eff4caad1570d15fc15ada52
trevorxarcieri <agent.steel.ta@gmail.com>
2025-06-02 08:50:29 -0400

    Updated esp32 stubs, removed now unnecessary pyright ignores.

--- tests/esp32/hil/blink/blink.py
+++ tests/esp32/hil/blink/blink.py
@@ -9,9 +9,9 @@ NUM_PIXELS = 1  # there is only one on-board LED
 np = neopixel.NeoPixel(Pin(PIXEL_PIN, Pin.OUT), NUM_PIXELS)
 
 while True:
-    np[0] = (50, 0, 0)  # pyright: ignore[reportIndexIssue] moderate-intensity red  (R,G,B)
+    np[0] = (50, 0, 0)  # moderate-intensity red  (R,G,B)
     np.write()
     time.sleep(0.5)
-    np[0] = (0, 0, 0)  # pyright: ignore[reportIndexIssue] off
+    np[0] = (0, 0, 0)  # off
     np.write()
     time.sleep(0.5)
