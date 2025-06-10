08e600a3933ff30b96536528b0669aae5ae5ac55
trevorxarcieri <agent.steel.ta@gmail.com>
2025-06-10 17:33:39 -0400

    Began implementation of voting tests.

---
+++ tests/esp32/hil/periodic_vote/main.py
@@ -0,0 +1,27 @@
+import neopixel
+from machine import Pin
+
+from controller import vote
+
+PIXEL_PIN = 8  # GPIO your board uses for the NeoPixel
+NUM_PIXELS = 1  # there is only one on-board LED
+
+np = neopixel.NeoPixel(Pin(PIXEL_PIN, Pin.OUT), NUM_PIXELS)
+
+on = False
+
+
+def rx_callback(payload: bytes) -> None:
+    """Callback function to handle received messages."""
+    print("Received:", payload.decode('utf-8'))
+    if not on:
+        np[0] = (50, 0, 0)  # moderate-intensity red  (R,G,B)
+        np.write()
+    else:
+        np[0] = (0, 0, 0)  # off
+        np.write()
+
+
+voter = vote.BleVoteController(name="ESP32-A", on_rx=rx_callback)
+voter.vote_yes()  # or .vote_no()
+voter.send("ACK")  # arbitrary string/bytes
---
+++ tests/rp2/hil/receive_votes/main.py
@@ -0,0 +1,17 @@
+import machine
+import vote
+
+led = machine.Pin("LED", machine.Pin.OUT)  # onboard LED = GP25
+
+
+def got_vote(node_id: int, payload: bytes) -> None:
+    """Callback function to handle received votes."""
+    print("Node", node_id, "sent", payload)
+    led.toggle()
+
+
+vc = vote.BleVoteManager(on_rx=got_vote, max_peers=5)
+vc.auto_connect()  # start scanning/connecting in background
+
+while True:
+    pass
